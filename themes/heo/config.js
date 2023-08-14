71659b945188fa75bf750e08bcf869ca553419c3
安紫泱 <ghstaytrue@users.noreply.github.com>
2023-08-15 00:50:57 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,8 +5,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://chancesha.com/about' },
-    { title: '访问文档中心获取更多帮助', url: 'https://www.chancesha.com/links' }
+    { title: 'chancesha.cn', url: 'https://chancesha.cn' },
+    { title: '欢迎友链！', url: 'https://www.chancesha.com/links' }
   ],
 
   // 英雄区(首页顶部大卡)
